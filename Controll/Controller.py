601d8540a58e854ecee5e92b6e38c0cbd7ba327d
aossuper7 <aossuper7@naver.com>
2023-01-22 22:22:55 +0900

    feat: progressBar 구현 완료

--- Controll/Controller.py
+++ Controll/Controller.py
@@ -3,22 +3,30 @@ import View.GetInfoWindow as GetInfoWindow
 from PyQt5.QtWidgets import QApplication
 from Model import YoutubeInfo
 from threading import Thread
+from time import sleep
 import sys
 
 
 class mainControll:
     def __init__(self):
         self.mainWindow = MainWindow.MainWindows(self)
+        self.info = None
+        self.value = 0
 
     def downloadClickEvent(self, parent):
-        info = GetInfoWindow.GetInfo(parent)
-        youtube = YoutubeInfo.YoutubeInfo(info)
-        self.startThread(youtube.loadPictureList)
+        self.info = GetInfoWindow.GetInfo(parent)
+        youtube = YoutubeInfo.YoutubeInfo(self.info, self)
+        Thread(target=youtube.loadPictureList, daemon=True).start()
 
-    def startThread(self, methodAddress):
-        t1 = Thread(target=methodAddress)
-        t1.daemon = True
-        t1.start()
+    def loadingBar(self, value, time):
+        self.value += value
+        for i in range(self.info.progressBar.value(), self.value + 1):
+            self.info.progressBar.setValue(i)
+            sleep(time)
+
+        if self.value == 100:
+            self.info.close()
+            self.value = 0
 
 
 if __name__ == '__main__':
--- Model/YoutubeInfo.py
+++ Model/YoutubeInfo.py
@@ -1,24 +1,28 @@
 import pytube
 import pyperclip
 from PyQt5.QtWidgets import QMessageBox
+from threading import Thread
 
 
 class YoutubeInfo:
-    def __init__(self, youtubeInfoSearchWindow):
+    def __init__(self, youtubeInfoSearchWindow, main):
         self.picture = []
         self.youtube = None
-        self.checkLink(youtubeInfoSearchWindow)
+        self.youtubeInfoSearchWindow = youtubeInfoSearchWindow
+        self.main = main
+        self.checkLink()
 
-    def checkLink(self, youtubeInfoSearchWindow):
+    def checkLink(self):
         try:
             self.youtube = pytube.YouTube(pyperclip.paste())
             self.youtube.check_availability()  # youtube 링크 체크
         except:
-            QMessageBox.warning(youtubeInfoSearchWindow, '8k Youtube downloader', '잘못된 링크 입니다.')
-            youtubeInfoSearchWindow.close()
+            QMessageBox.warning(self.youtubeInfoSearchWindow, '8k Youtube downloader', '잘못된 링크 입니다.')
+            self.youtubeInfoSearchWindow.close()
 
     def loadPictureList(self):
         resolution = ['4320p', '2160p', '1440p', '1080p', '720p', '480p', '360p', '240p']
+        Thread(target=self.main.loadingBar, args=(99, 0.13), daemon=True).start()
         for i in range(len(resolution)):
             youtube = self.youtube.streams\
                                     .filter(mime_type='video/mp4', progressive=False, res=resolution[i])\
@@ -26,6 +30,7 @@ class YoutubeInfo:
                                     .asc()
             if youtube is not None:
                 self.picture.append(youtube)
+        self.main.loadingBar(1, 0)
 
     def getPicture(self):
         return self.picture
